4bdadac19cb102aebfecf022f6dfbb201bfb4198
Sniedze Spele <spele.sniedze@gmail.com>
2021-01-07 10:55:48 +0200

    finish js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-
-
 document.querySelector(".dropdown").addEventListener("click", function () {
-  document.querySelector('.side-menu').classList.add("active");
-  if (this.classList.contains("active")) {
-    this.classList.remove("active");
-  } else {
-    this.classList.add("active");
-  }
-});
 
-//dropdown pieliek un noņem klasi active, bet side menu tikai pieliek.
+  document.querySelector(".side-menu").classList.toggle("active");
+});
